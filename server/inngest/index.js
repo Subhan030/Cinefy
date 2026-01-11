2bfe3775b548dfe60190278946578ea7d956431c
Subhan030 <raisubhan728@gmail.com>
2026-01-11 15:48:06 +0530

    Added Backend Models

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -7,7 +7,7 @@ export const inngest = new Inngest({ id: "my-app" });
 const syncUserCreation = inngest.createFunction(
     { id: 'sync-user-from-clerk' },
     { event: 'clerk/user.created' },
-    async (event) => {
+    async ({ event }) => {
         const { id, first_name, last_name, email_addresses, image_url } = event.data
         const userData = {
             _id: id,
@@ -23,7 +23,7 @@ const syncUserCreation = inngest.createFunction(
 const syncUserDeletion = inngest.createFunction(
     { id: 'delete-user-from-clerk' },
     { event: 'clerk/user.deleted' },
-    async (event) => {
+    async ({ event }) => {
         const { id } = event.data
         await User.findByIdAndDelete(id)
         return { success: true }
@@ -33,7 +33,7 @@ const syncUserDeletion = inngest.createFunction(
 const syncUserUpdation = inngest.createFunction(
     { id: 'update-user-from-clerk' },
     { event: 'clerk/user.updated' },
-    async (event) => {
+    async ({ event }) => {
         const { id, first_name, last_name, email_addresses, image_url } = event.data
         const userData = {
             _id: id,
