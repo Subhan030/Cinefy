21dc55e04482647d168bfd9de584693751bf5fe0
Subhan030 <raisubhan728@gmail.com>
2026-01-11 14:56:58 +0530

    Added Backend Models

--- server/server.js
+++ server/server.js
@@ -18,4 +18,9 @@ app.use(clerkMiddleware())
 app.get('/', (req, res) => res.send('Server is Live!'))
 app.use("/api/inngest", serve({ client: inngest, functions }));
 
-app.listen(port, () => console.log(`Server listening at http://localhost:${port}`));
+// Export the app for Vercel
+export default app;
+
+if (process.env.NODE_ENV !== 'production') {
+    app.listen(port, () => console.log(`Server listening at http://localhost:${port}`));
+}
